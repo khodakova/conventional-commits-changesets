1fbd603ce4811cfdbaecd6d1d2578e1086451228
Khodakova <Khodakova@lanit.ru>
2024-02-21 19:12:11 +0300

    feat(conventional-changeset): vitest added

--- packages/conventional-changeset/__tests__/get-conventional-commit-changelog.test.ts
+++ packages/conventional-changeset/__tests__/get-conventional-commit-changelog.test.ts
@@ -3,7 +3,7 @@ import { Changeset } from '@changesets/types';
 import { getConventionalChangelog } from '../src';
 
 const testPackage: Changeset = {
-  releases: [{ name: '@ebp/test-utils', type: 'patch' }],
+  releases: [{ name: '@repo/eslint-config', type: 'patch' }],
   summary: '123',
 };
 
